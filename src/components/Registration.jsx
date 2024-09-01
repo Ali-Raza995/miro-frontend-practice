2f2a7dbacb6bb97ba96e441396b9d0550fd5a8a1
Ali Raza <alirazaarif995@gmail.com>
2024-09-02 01:03:23 +0500

    Updates

---
+++ src/components/Registration.jsx
@@ -0,0 +1,11 @@
+
+// eslint-disable-next-line react/prop-types
+const Registration = ({ defaultValue }) => {
+  return (
+    <div>
+      <p>Registration Component with default value: {defaultValue}</p>
+    </div>
+  );
+};
+
+export default Registration;
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "myMicrofrontend",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Registration": "./src/components/Registration.jsx",
+      },
+      shared: ["react", "react-dom"],
+    })
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+});
